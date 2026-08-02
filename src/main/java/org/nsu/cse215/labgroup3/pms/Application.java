da1245e6b35da0c892ecc04b83cd14f87404d969
Ar Rakin <rakinar2@onesoftnet.eu.org>
2026-08-02 14:17:21 +0600

    feat: add darwin support
    Signed-off-by: Ar Rakin <rakinar2@onesoftnet.eu.org>

--- src/main/java/org/nsu/cse215/labgroup3/pms/Application.java
+++ src/main/java/org/nsu/cse215/labgroup3/pms/Application.java
@@ -136,7 +136,7 @@ public class Application extends javafx.application.Application {
 
         rootBox.getStyleClass().add("rootBox");
         scene.getStylesheets().add(Objects.requireNonNull(Thread.currentThread().getContextClassLoader().getResource("ui/global.css")).toExternalForm());
-        stackPane.getChildren().add(findScene.getRoot());
+        stackPane.getChildren().add(homeScene.getRoot());
 
         stage.setScene(scene);
         stage.show();
--- src/main/java/org/nsu/cse215/labgroup3/pms/Main.java
+++ src/main/java/org/nsu/cse215/labgroup3/pms/Main.java
@@ -1,7 +1,10 @@
 package org.nsu.cse215.labgroup3.pms;
 
+import javax.xml.transform.TransformerException;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, TransformerException {
         Application.launch(org.nsu.cse215.labgroup3.pms.Application.class, args);
     }
 }
